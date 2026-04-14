6683b204efc5738a85a698e960b0f7e35551b86e
Anuj <anuj89806@gmail.com>
2026-04-15 00:52:40 +0530

    fixed bug and dashboard setup

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,7 +1,11 @@
-import React from "react";
-
 const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+  return (
+    <div>
+      <div className="border p-4 rounded-full hover:text-muted cursor-text">
+        <p className="text-muted-foreground">What are you building today?</p>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardPage;
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,13 @@
+import Container from "@/components/global/Container";
+import Navbar from "@/components/navbar/Navbar";
+
 const DashbaordLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <>
+      <Navbar />
+      <Container className="py-20">{children}</Container>
+    </>
+  );
 };
 
 export default DashbaordLayout;
